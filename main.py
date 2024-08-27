1c2890760fddcbcc0816716d59b234c849a10eae
irvind <irvind25@gmail.com>
2024-08-27 17:35:02 +0000

    Update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import time
+import random
 
 import requests
 
@@ -18,6 +19,13 @@ class Task(BaseModel):
     typeof: TaskType
 
 
+class InTaskCoefs(BaseModel):
+    typeof: TaskType
+    cpu: float
+    gpu: float
+    network: float
+
+
 app = FastAPI()
 
 DEFAULT_COEFS = {'cpu_bound': {'cpu': 1.0,
@@ -29,9 +37,13 @@ DEFAULT_COEFS = {'cpu_bound': {'cpu': 1.0,
                  'gpu_bound': {'cpu': 0.0,
                                'network': 0.0,
                                'gpu': 1.0}}
+WORKER1_URL = 'http://192.168.31.49:8000'
+WORKER2_URL = 'http://192.168.31.48:8000'
+
 TASK_COEFS = {}
-WORKER_URLS = ['http://192.168.31.49:8000', 'http://192.168.31.48:8000']
+WORKER_URLS = [WORKER1_URL, WORKER2_URL]
 WORKER_STATE: int | None = [None for _ in range(len(WORKER_URLS))]
+WORKER_NAME_MAP = {'http://192.168.31.49:8000': 'first (49)', 'http://192.168.31.48:8000': 'second (48)'} 
 
 
 def init_coefs():
@@ -77,40 +89,49 @@ def argmax(it):
     return max_idx
 
 
-def fetch_worker_state():
+def fetch_worker_state() -> None:
+    """
+    Получаем информацию о текущей нагрузке воркеров и сохраняем её в памяти.
+    """
     for idx, url in enumerate(WORKER_URLS):
-        WORKER_STATE[idx] = {
-            'cpu_perc': 80 + idx*10,
-            'gpu_perc': 0,
-            'available_RAM': 10000,
-            'available_storage': 1000*1000*100
-        }
         resp = requests.get(url + '/server/load')
         body_json = resp.json()
+        # print(body_json)
         WORKER_STATE[idx] = {'cpu_perc': body_json['available_FLOPS_percentage'],
-                             'gpu_perc': 0,
-                            'available_RAM': body_json['available_RAM']}
+                             'gpu_perc': body_json['available_gpu_FLOPS_percentage'],
+                             # TODO: замокать по другому
+                             'net_delay': random.random(),
+                             'available_RAM': body_json['available_RAM']}
+        print(f"WORKER_STATE {url}: {WORKER_STATE[idx]}")
 
 
-def pick_worker(task_type):
+def pick_worker(task_type: str) -> int:
+    """
+    Возвращаем индекс воркера, на котором будем выполнять задачу.
+    """
     coefs = get_task_coefs()[task_type]
     fetch_worker_state()
-    k_storage = 1
-    k_ram = 1
+
+    k_storage, k_ram = 1, 1
     scores = []
     for idx, w_state in enumerate(WORKER_STATE):
         score =\
             coefs['cpu'] * (w_state['cpu_perc'] / 100) +\
             coefs['gpu'] * (w_state['gpu_perc'] / 100) +\
-            coefs['network'] * 1
+            coefs['network'] * (w_state['net_delay'])
         score = score * k_storage * k_ram
         scores.append(score)
+        print(f'Worker score "{WORKER_URLS[idx]}": {score}')
 
     worker_idx = argmax(scores)
+
     return worker_idx
 
 
-def run_task_in_worker(worker_idx, task_type):
+def run_task_in_worker(worker_idx: int, task_type: str) -> dict: 
+    """
+    Запускаем задачу на конкретном воркере и возвращаем тело ответа.
+    """
     task_mapping = {'cpu_bound': 'kr1t1ka/cpu-bound',
                     'gpu_bound': 'kr1t1ka/gpu-bound',
                     'network_bound': 'kr1t1ka/network-bound'}
@@ -131,11 +152,22 @@ def make_new_task(task: Task):
     time_run_start = time.time()
     worker_idx = pick_worker(task.typeof.value)
     time_run_task_start = time.time()
-    res = run_task_in_worker(worker_idx,'cpu_bound')
-    print(f"Time with pick_worker: {time.time() - time_run_start}")
-    print(f"Time with out pick_worker: {time.time() - time_run_task_start}")
-    print(f"Time real: {res['logs']['execution_time']}")
-    return res
+    task_res = run_task_in_worker(worker_idx,'cpu_bound')
+
+    pick_worker_time = time.time() - time_run_start
+    without_pick_worker_time = time.time() - time_run_task_start
+    real_time = task_res['logs']['execution_time']
+
+    meta_resp = {'pick_worker_time': pick_worker_time,
+                 'without_pick_worker_time': without_pick_worker_time,
+                 'real_time': real_time,
+                 'worker_name': WORKER_NAME_MAP[WORKER_URLS[worker_idx]]}
+    
+    #print(f"Time with pick_worker: {pick}")
+    #print(f"Time with out pick_worker: {time.time() - time_run_task_start}")
+    #print(f"Time real: {res['logs']['execution_time']}")
+    print(meta_resp)
+    return meta_resp
 
 
 @app.post("/api/v1/stop_all")
@@ -148,7 +180,15 @@ def stop_all_workers():
     return {'status': 'ok'}
 
 
-@app.get('/api/v1/conf')
-def get_conf_handler():
+@app.get('/api/v1/coefs')
+def get_coefs_handler():
     return get_task_coefs()
+
+
+@app.post('/api/v1/coefs')
+def set_coefs_handler(coefs: InTaskCoefs):
+    TASK_COEFS[coefs.typeof] = {'cpu': coefs.cpu,
+                                'gpu': coefs.gpu,
+                                'network': coefs.network}
+    return {'status': 'ok'}
 
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 if [[ -f .env ]]; then
     set -o allexport && . .env && set +o allexport
 fi
-uvicorn main:app --reload
+uvicorn main:app --host 0.0.0.0 --port 8001 --reload
